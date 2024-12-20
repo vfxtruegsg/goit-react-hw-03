f02530bfde23c3c84b1a6da1bee339ac94634b9a
Andrij P. <andrejpopkov565@gmail.com>
2024-12-20 13:07:20 +0300

    hw3

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,47 @@
 import "./App.css";
-import Profile from "./components/Profile/Profile";
-import FriendList from "./components/FriendList/FriendList";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
-import userData from "./components/Profile/userData.json";
-import allFriend from "./components/FriendList/friends.json";
-import transactions from "./components/TransactionHistory/transactions.json";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactForm from "./components/ContactForm/ContactForm";
+import { useEffect, useState } from "react";
 
 function App() {
+  const arrayContacts = [
+    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  ];
+
+  const [phone, setPhone] = useState(
+    JSON.parse(localStorage.getItem("contacts"))
+  );
+  useEffect(() => {
+    localStorage.setItem("contacts", JSON.stringify(phone));
+  });
+  const addTask = (newTask) => {
+    setPhone((prevTasks) => {
+      return [...prevTasks, newTask];
+    });
+  };
+
+  const [findName, setFindName] = useState("");
+
+  const visibleTasks = phone.filter((task) =>
+    task.name.toLowerCase().includes(findName.toLowerCase())
+  );
+
+  const deleteNumber = (numberId) => {
+    setPhone((prevItem) => {
+      return prevItem.filter((item) => item.id !== numberId);
+    });
+  };
+
   return (
     <>
-      <Profile userData={userData} />
-      <FriendList friends={allFriend} />
-      <TransactionHistory items={transactions} />
+      <h1>Phonebook</h1>
+      <ContactForm contactsArray={addTask} />
+      <SearchBox value={findName} onFindName={setFindName} />
+      <ContactList arrayContacts={visibleTasks} deleteNumber={deleteNumber} />
     </>
   );
 }
